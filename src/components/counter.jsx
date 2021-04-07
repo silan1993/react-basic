9d85a779dab9b7bcbe44a21a66a0ca97b9ba80bc
silansubhra <subhrajyoti1903@gmail.com>
2021-04-08 00:15:04 +0530

    play with state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,15 +2,45 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    tags: ["tag1", "tag2", "tag3"],
+  };
+
+  constructor() {
+    super();
+    this.handleIncrement.bind(this);
+  }
+  renderTags() {
+    if (this.state.tags.length == 0) return <p>'there are no tags'</p>;
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
   };
   render() {
     return (
       <div>
-        <span>{this.formatCount()}</span>
-        <button>Incerement</button>
+        <span style={this.getBatchColor()}>{this.formatCount()}</span>
+        <button onClick={this.handleIncrement}>Incerement</button>
+        {this.renderTags()}
       </div>
     );
   }
+  getBatchColor() {
+    let style = {
+      fontSize: 30,
+      fontWeight: "bold",
+      backgroundColor: "yellow",
+    };
+    style.backgroundColor = this.state.count == 0 ? "yellow" : "blue";
+    return style;
+  }
+
   formatCount() {
     const { count } = this.state;
     return count == 0 ? "Zero" : count;
